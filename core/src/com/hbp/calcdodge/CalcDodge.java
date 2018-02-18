3381399642097ec161b44bbcbc765296089c198c
Ben Panton <abstraction.application@protonmail.com>
2018-02-18 15:55:29 +0000

    first of all

---
+++ core/src/com/hbp/calcdodge/CalcDodge.java
@@ -0,0 +1,30 @@
+
+package com.hbp.calcdodge;
+
+/*
+ * ~SUMMARY~
+ * 
+ * This is the actual game.
+ * It's pretty empty: all the fun happens in the screens, which we hand off to as soon as possible.
+ * 
+ */
+
+import com.badlogic.gdx.Game;
+
+
+public class CalcDodge extends Game {
+	
+	public void create() {
+		
+		this.setScreen(new TitleScreen(this, false)); //Hand off to title screen.
+		
+	}
+
+	public void render() {
+		super.render(); // I deleted this once and I deeply regretted it.
+	}
+
+	public void dispose() {
+	}
+
+}
---
+++ desktop/src/com/aa/hbp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aa.hbp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hbp.calcdodge.CalcDodge;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CalcDodge(), config);
+	}
+}
